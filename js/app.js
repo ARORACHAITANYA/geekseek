624c0c96da81eb710735b1d43fdc0208f2e7e015
sachin <iamsainisachin@gmail.com>
2017-12-26 00:46:48 +0530

    add responsive navbar

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var toggler = document.querySelector(".toggler");
+
+var nav = document.querySelector(".navigation");
+
+toggler.addEventListener("click", function(){
+    nav.classList.toggle("isvisible");
+});
+
+window.addEventListener("resize", function(){
+    if(nav.classList.contains("isvisible") && window.innerWidth > 768){
+        nav.classList.remove("isvisible");
+    }
+});
